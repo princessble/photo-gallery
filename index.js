6509159377204d2bff397e5349a23b10a1610d75
princessble <kwomofortuneb@gmail.com>
2026-02-23 14:55:29 +0000

    console.log data

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const btnEl = document.getElementById("btn")
 
 
 function fetchImage(){
-    console.log("clicked")
+    const inputValue = document.getElementById("input").value
+    fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1`).then((res)=>res.json().then((data)=>{
+        console.log(data);
+    }))
 }
 btnEl.addEventListener("click", fetchImage)
