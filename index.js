9fc90df06c2a96c29627acba29f5505192999812
princessble <kwomofortuneb@gmail.com>
2026-02-23 19:04:19 +0000

    add the input value of > 10

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const btnEl = document.getElementById("btn")
 
 
 function fetchImage(){
-    const inputValue = document.getElementById("input").value
+    const inputValue = document.getElementById("input").value;
+
+    if(inputValue > 10 || inputValue < 1){
+        
+    }
     fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).then((res)=>res.json().then((data)=>{
         console.log(data);
     }))
