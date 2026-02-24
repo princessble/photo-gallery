671a40a2eb669000fb5ed4c5162e33320e970cc0
princessble <kwomofortuneb@gmail.com>
2026-02-24 12:15:38 +0000

    update const errorMessage

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const btnEl = document.getElementById("btn")
+const errorMessageE1 = document.getElementById("errorMessage")
+
 
 
 function fetchImage(){
     const inputValue = document.getElementById("input").value;
 
     if(inputValue > 10 || inputValue < 1){
-        
+        errorMessageE1.style.display = "block"
+
     }
     fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).then((res)=>res.json().then((data)=>{
         console.log(data);
