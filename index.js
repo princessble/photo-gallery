825920faf97f3839f0b36d264368320f24e88151
princessble <kwomofortuneb@gmail.com>
2026-02-24 12:52:30 +0000

    update async

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const errorMessageE1 = document.getElementById("errorMessage")
 
 
 
-function fetchImage(){
+async function fetchImage(){
     const inputValue = document.getElementById("input").value;
 
     if(inputValue > 20 || inputValue < 1){
@@ -12,7 +12,7 @@ function fetchImage(){
         return
 
     }
-    fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).then((res)=>res.json().then((data)=>{
+    await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).then((res)=>res.json().then((data)=>{
         console.log(data);
     }))
 }
