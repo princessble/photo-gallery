42d02b92649db2aef91f1e39790c5d2d3ae1902d
princessble <kwomofortuneb@gmail.com>
2026-02-24 14:01:02 +0000

    add catch error

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ async function fetchImage(){
     if(inputValue > 20 || inputValue < 1){
         errorMessageE1.style.display = "block";
         errorMessageE1.innerText = "number should be between 0 and 21"
-        return
+        return;
 
     }
-    await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).then((res)=>res.json().then((data)=>{
+
+    try {
+        await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).
+
+    .then((res)=>res.json().then((data)=>{
         console.log(data);
-    }))
+    }));
+    errorMessageE1.style.display = "none" ;-
+} catch (error) {
+
+}
+
 }
 btnEl.addEventListener("click", fetchImage)
