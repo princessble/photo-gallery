ff3c17bce0cab073199cd2d7d7bb43e4fefe3347
princessble <kwomofortuneb@gmail.com>
2026-02-24 12:19:22 +0000

    add return request at input value > 20

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const errorMessageE1 = document.getElementById("errorMessage")
 function fetchImage(){
     const inputValue = document.getElementById("input").value;
 
-    if(inputValue > 10 || inputValue < 1){
-        errorMessageE1.style.display = "block"
+    if(inputValue > 20 || inputValue < 1){
+        errorMessageE1.style.display = "block";
+        return
 
     }
     fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).then((res)=>res.json().then((data)=>{
