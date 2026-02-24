1c6563d1e2888af53afbc655b47d1d38727375a7
princessble <kwomofortuneb@gmail.com>
2026-02-24 14:07:50 +0000

    update await and .then

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const btnEl = document.getElementById("btn")
-const errorMessageE1 = document.getElementById("errorMessage")
+const btnEl = document.getElementById("btn");
+const errorMessageEl = document.getElementById("errorMessage");
 
+async function fetchImage() {
+  const inputValue = document.getElementById("input").value;
 
+  if (inputValue > 20 || inputValue < 1) {
+    errorMessageEl.style.display = "block";
+    errorMessageEl.innerText = "Number should be between 1 and 20";
+    return;
+  }
 
-async function fetchImage(){
-    const inputValue = document.getElementById("input").value;
+  try {
+    const res = await fetch(
+      `https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`
+    );
 
-    if(inputValue > 20 || inputValue < 1){
-        errorMessageE1.style.display = "block";
-        errorMessageE1.innerText = "number should be between 0 and 21"
-        return;
+    const data = await res.json();
+    console.log(data);
 
-    }
-
-    try {
-        await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=vQ1ZD-c6ZJO_P_i0DIj0IRkNXfabmwygdgBwbFkBiaA`).
-
-    .then((res)=>res.json().then((data)=>{
-        console.log(data);
-    }));
-    errorMessageE1.style.display = "none" ;-
-} catch (error) {
-
+    errorMessageEl.style.display = "none";
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    errorMessageEl.style.display = "block";
+    errorMessageEl.innerText = "Something went wrong. Try again!";
+  }
 }
 
-}
-btnEl.addEventListener("click", fetchImage)
+btnEl.addEventListener("click", fetchImage);
